a585ba0115def8bad5c74aa0f3ba239da82ff102
Arthur Smalle <arthur@smalle.be>
2025-10-16 19:37:15 +0200

    Added implementation for 2 IR sensor angle predection Refactored platformio config to have general build factors that are used in all env (less double statements and easier to port to other mcu)

--- src/angle control/IRsensorPrediction.h
+++ src/angle control/IRsensorPrediction.h
@@ -9,6 +9,35 @@ class IRSensorPrediction{
   
   private:
     float last_prediction = 0;
+    uint8_t ir_array[IR_AMOUNT] = {0};
+
+    void get_ir_readings(){
+      ir_array[0] = digitalRead(IR_PIN0);
+      ir_array[1] = digitalRead(IR_PIN1);
+      #if IR_AMOUNT >= 2
+      ir_array[2] = digitalRead(IR_PIN2);
+      #endif
+    }
+
+  // TODO continue working here  
+  #if IR_AMOUNT == 2
+  public:
+    float predict_angle(){
+      float prediction = 0;
+      if (ir_array[0] == HIGH){
+	prediciton = -1 * tan(SYMM_TO_IR/WHEELS_TO_IR);
+	last_prediction = prediction;
+      } 
+      else if (ir_array[1] == HIGH){
+	prediciton = tan(SYMM_TO_IR/WHEELS_TO_IR);
+	last_prediction = prediction;
+      }
+
+      return prediction;
+      }
+    }
+  private:
+  #endif
 
   #if IR_AMOUNT == 3
   // TEMP CODE REWORK THIS LATER
@@ -31,14 +60,6 @@ class IRSensorPrediction{
     }
 
   private:
-    uint8_t ir_array[3] = {0};
-
-    void get_ir_readings(){
-      ir_array[0] = digitalRead(IR_PIN0);
-      ir_array[1] = digitalRead(IR_PIN1);
-      ir_array[2] = digitalRead(IR_PIN2);
-    }
-
   #endif // end 3 ir led logic
 };
 #endif
