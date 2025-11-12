62cb8d4bb2fe296e80731397ad97d66e51fd4a16
Arthur Smalle <arthur@smalle.be>
2025-11-12 19:33:50 +0100

    Added testing function to determin angular acceleration based on ir sensor going to a line (NOT TESTED YET)

--- src/robot states/TSAngMomMeasure.h
+++ src/robot states/TSAngMomMeasure.h
@@ -11,7 +11,35 @@ class TSAngMomMeasure : public RobotState{
 
   protected:
     // Add variables here
+    unsigned long set_point_list[AMOUNT_OF_MEASUREMENTS];
     unsigned long measurement_list[AMOUNT_OF_MEASUREMENTS] = {};
+
+    void single_motor_test(unsigned long measurement_list[], const int measurement_list_size){
+      motor_cl_l.stop();
+      motor_cl_r.stop();
+      // update while not triggering the ir sensor
+      for ( int i = 0; i < measurement_list_size; i++){
+	// reset to take new measurement
+	motor_cl_r.set_set_point(20);
+	motor_cl_r.enable();
+	motor_cl_r.set_clock_wise(false);
+	while(!angle_controller.get_ir_triggered()){
+      	  RobotState::update();
+	}
+	motor_cl_r.set_clock_wise(true);
+        
+	// take measurements
+	unsigned long start_time = micros();
+      	motor_cl_r.enable();
+	motor_cl_r.set_set_point(set_point_list[i]);
+
+	while(!angle_controller.get_ir_triggered()){
+      	  RobotState::update();
+      	}
+	measurement_list[i] = start_time - micros();
+      	motor_cl_r.stop();
+      }
+    }
 
     void enter() override{
       motor_cl_l.set_set_point(0);
@@ -22,19 +50,16 @@ class TSAngMomMeasure : public RobotState{
 
     // keep looping
     void update() override{
-      float last_angle = this->get_angle();
-      RobotState::update(); // roep dit aan (enkel in update) om de angle telkens up te daten
-      // detect if ir sens is triggered
-      if (angle_controller.get_ir_triggered()){
-	motor_cl_l.stop();
-	motor_cl_r.stop();
-      } else {
-	// detect overshooting
-	if (last_angle != this->get_angle()){
-	  // TODO IMPLEMENT CORRECTION FOR OVERSHOOTING
-	}
+      // generate setpoint list
+      for (int i = 0; i < AMOUNT_OF_MEASUREMENTS; i++){
+	set_point_list[i] = i + 20;
       }
-
+      single_motor_test(this->measurement_list, AMOUNT_OF_MEASUREMENTS);
+      digitalWrite(13, HIGH);
+      for (int i = 0; i < AMOUNT_OF_MEASUREMENTS; i++){
+	Serial.println("set point: " + String(set_point_list[i]));
+	Serial.println("measurement: " + String(measurement_list[i]));
+      }
     }
 
     State * go_next_state() override{}
