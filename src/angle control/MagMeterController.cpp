14c674e8f1cce18aa94b4b951c8c6f67414625e5
Arthur Smalle <arthur@smalle.be>
2025-11-21 08:32:38 +0100

    Adding functionality to MagMeterController

--- src/angle control/MagMeterController.cpp
+++ src/angle control/MagMeterController.cpp
@@ -4,6 +4,9 @@ float MagMeterController::get_angle_to_zero(){
   if (!angle_ref_set || !zp_set){
     return 0;
   } else {
-
+    if (!angle_to_tan_scale_set){
+      calculate_angle_to_tan_scale();
+    }
+    return tan(x_zp - x/y_zp - y) * angle_to_tan_scale;
   }
 }
--- src/angle control/MagMeterController.h
+++ src/angle control/MagMeterController.h
@@ -1,16 +1,18 @@
 #ifndef MAGMETERCONTROLLER_H
 #define MAGMETERCONTROLLER_H
+#include <Arduino.h>
 
 class MagMeterController{
   public:
     float get_angle_to_zero();
     int virtual get_mag_data(float &x,float &y,float &z);
     bool virtual is_mag_ready();
-    void virtual set_angle_to_magdata_ref(const float rad, const float x,const float y,const float z){
+    void set_angle_to_magdata_ref(const float rad, const float x,const float y,const float z){
       this->angle_ref = rad;
       this->x_angle_ref =x;
       this->y_angle_ref =y;
       this->z_angle_ref =z;
+      this->angle_to_tan_scale_set = false;
     }
     
     bool get_angle_ref_set(){return  this->angle_ref_set;}
@@ -23,14 +25,27 @@ class MagMeterController{
       this->y_zp = y;
       this->z_zp = z;
       this->zp_set = true;
+      this->angle_to_tan_scale_set = false;
     } 
   private:
+    void calculate_angle_to_tan_scale(){
+      if (!angle_ref_set || !zp_set){
+	angle_to_tan_scale = 0;
+      } else {
+	float angle = tan ((this->x_zp - this->x_angle_ref) / (this->y_zp - this->y_angle_ref));
+	angle_to_tan_scale = angle_ref / angle;
+	angle_to_tan_scale_set = true;
+      }
+    }
+
     bool angle_ref_set = false; // keep track if angle ref is set or not
     bool zp_set = false; // keep track if the zero point is already set or not
+    bool angle_to_tan_scale_set = false; // (angle / tan(x/y))
     float x,y,z; // working data
     float x_zp, y_zp, z_zp; // zero point data
     // refrence data for angle to magdata
     float angle_ref;
     float x_angle_ref, y_angle_ref, z_angle_ref;
+    float angle_to_tan_scale = 0; // (angle / tan(x/y))
 };
 #endif
