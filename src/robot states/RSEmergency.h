4230cc6c938f50b240336a6a910335fb6163fa71
Arthur Smalle <arthur@smalle.be>
2025-12-21 14:49:23 +0100

    Last minute change to emergency state now just drives forward and ignores sensor for x amount of time (usefull when the line to follow is interrupted by blanks)

--- src/robot states/RSEmergency.h
+++ src/robot states/RSEmergency.h
@@ -4,21 +4,37 @@
 #include "state machine/StatesEnum.h"
 #include <Arduino.h>
 
+
 class RSEmergency : public RobotState{
   public:
+    unsigned long emergency_start_time;
     RSEmergency(){
     }
 
   protected:
     // do something on start of the state (only does this once)
     void enter() override{
-      angle_controller.emergency_flip();
+      emergency_start_time = millis();
+       motor_cl_l.set_set_point(250);
+       motor_cl_r.set_set_point(250);
     }
 
     // keep looping
     void update() override{
-      this->next_ready = true;
+      // init flipper
+      float flipper = 10;
+      // set motorspeed
+       motor_cl_l.set_set_point(250 + flipper);
+       motor_cl_r.set_set_point(250 - flipper);
       RobotState::update(); // roep dit aan (enkel in update) om de angle telkens up te daten
+      // make sure robot drives forward
+      if (millis() - emergency_start_time > 100){
+	flipper *= -1;
+      }
+      // exit emergency_state
+      if (millis() - emergency_start_time > 2000){
+	next_ready = true;
+      }
 #ifdef DEBUG
   Serial.println("$$P-auto,00,00,00,00,50");
   // note: 50 is code for this state (usefull to see in plotting software)
